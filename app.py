a99b375c3f9559f5b270578fc17ada7e682990b1
RichaG7 <RichaG7>
2020-04-18 18:38:16 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,61 @@
+# 1. import Flask
+from flask import Flask, jsonify
+
+# 2. Create the superhero dictionary
+justice_league_members = [
+    {"superhero": "Aquaman", "real_name": "Arthur Curry"},
+    {"superhero": "Batman", "real_name": "Bruce Wayne"},
+    {"superhero": "Cyborg", "real_name": "Victor Stone"},
+    {"superhero": "Flash", "real_name": "Barry Allen"},
+    {"superhero": "Green Lantern", "real_name": "Hal Jordan"},
+    {"superhero": "Superman", "real_name": "Clark Kent/Kal-El"},
+    {"superhero": "Wonder Woman", "real_name": "Princess Diana"}
+]
+
+# 3. Create an app, being sure to pass __name__
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/api/v1.0/justice-league")
+def justice_league():
+    """Return the justice league data as json"""
+
+    return jsonify(justice_league_members)
+
+
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Justice League API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/justice-league<br/>"
+        f"/api/v1.0/justice-league/Aquaman<br/>"
+        f"/api/v1.0/justice-league/Batman<br/>"
+        f"/api/v1.0/justice-league/Cyborg<br/>"
+        f"/api/v1.0/justice-league/Flash<br/>"
+        f"/api/v1.0/justice-league/Green%20Lantern<br/>"
+        f"/api/v1.0/justice-league/Superman<br/>"
+        f"/api/v1.0/justice-league/Wonder%20Woman"
+    )
+
+
+@app.route("/api/v1.0/justice-league/<superhero>")
+def justice_league_character(superhero):
+    """Fetch the Justice League character whose superhero name matches
+       the path variable supplied by the user, or a 404 if not."""
+
+    canonicalized = superhero.replace(" ", "").lower()
+    for character in justice_league_members:
+        search_term = character["superhero"].replace(" ", "").lower()
+
+        if search_term == canonicalized:
+            return jsonify(character)
+
+    return jsonify({"error": f"Character with superhero name {superhero} not found."}), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
