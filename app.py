13ded884b4e7d75dc4eaf01dcba8a2c72493ce8c
RichaG7 <RichaG7>
2020-06-16 21:20:28 -0400

    updated app as per jon's comments

--- app.py
+++ app.py
@@ -38,32 +38,32 @@ def welcome():
 def precipitation():
     """Return the precipitation data as json"""
 
-    return jsonify(session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= '2016-08-23').order_by(Measurement.date.desc()).all())
+    return jsonify(dict(session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= '2016-08-23').order_by(Measurement.date.desc()).all()))
 
 
 @app.route("/api/v1.0/stations")
 def station():
     """Return the station data as json"""
 
-    return jsonify(session.query(Measurement.station).group_by(Measurement.station).all())
+    return jsonify(dict(session.query(Measurement.station).group_by(Measurement.station).all()))
 
 @app.route("/api/v1.0/tobs")
 def tobs():
     """Return the temperature data from most active station as json"""
 
-    return jsonify(session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= '2016-08-18').order_by(Measurement.date.desc()).all())
+    return jsonify(dict(session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= '2016-08-18').order_by(Measurement.date.desc()).all()))
 
 @app.route("/api/v1.0/<start>")
 def start(start):
     """Fetch the minimum temperature, the average temperature, and the max temperature for dates after a given start date."""
     
-    return jsonify(session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).all())
+    return jsonify(dict(session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).all()))
 
 @app.route("/api/v1.0/<start>/<end>")
 def start_end(start, end):
     """Fetch the minimum temperature, the average temperature, and the max temperature for a given start or start-end range."""
 
-    return jsonify(session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all())
+    return jsonify(dict(session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()))
 
 
 if __name__ == "__main__":
